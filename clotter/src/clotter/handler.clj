567f65b9ff7a84043f38815d5824ed3d3ad05351
Rahul Acharya <ach@Smac.local>
2020-09-29 12:40:03 +0530

    feat(handler): Create handler for Tweets

--- clotter/src/clotter/handler.clj
+++ clotter/src/clotter/handler.clj
@@ -1,19 +1,10 @@
 (ns clotter.handler
-  (:require [compojure.api.sweet :refer :all]
-            [ring.util.http-response :refer :all]
-            [schema.core :as s]
-            [clojure.string :as str]
-            [cheshire.core :refer :all]
-            [clojure.data.json :as json]
+  (:require [cheshire.core :as json]
             [clj-http.client :as client]))
 
 (def ^:const RECENT-SEARCH-ENDPOINT "https://api.twitter.com/2/tweets/search/recent")
 
 (def ^:const ENV-BEARER-TOKEN (str "BEARER " (System/getenv "TWITTER_BEARER")))
-
-(s/defschema Tweet
-  {:id s/Str
-   :text s/Str})
 
 (defn tweets-collection [query-params max-results bearer-token]
   (let [response (client/get
@@ -28,26 +19,28 @@
   [user-name max-results bearer-token]
   (let [query-params (str "from:" user-name " -is:retweet")
         response     (tweets-collection query-params max-results bearer-token)]
-    (parse-string (get response :body))))
+    (json/parse-string (get response :body))))
 
 
-(def app
-  (api
-   {:swagger {:ui   "/"
-              :spec "/swagger.json"
-              :data {:info {:title       "Clotter"
-                            :description "Compojure Api example"}
-                     :tags [{:name        "api"
-                             :description "some apis"}]}}}
+;; (def app
+  ;; (api
+  ;;  {:swagger {:ui   "/"
+              ;; :spec "/swagger.json"
+              ;; :data {:info {:title       "Clotter"
+                            ;; :description "Compojure Api example"}
+                    ;;  :tags [{:name        "api"
+                            ;;  :description "some apis"}]}}}
+;; 
+  ;;  (context "/api" []
+    ;;  :tags ["api"]
+;; 
+    ;;  (GET "/tweets" []
+      ;;  :query-params [user-name    :- s/Str
+                      ;; {max-results :- s/Str 10}
+                      ;; {bearer-token :- s/Str ENV-BEARER-TOKEN}]
+      ;;  :summary "success"
+      ;;  (ok {:result (fetch-recent-tweets user-name max-results bearer-token)})))))
 
-   (context "/api" []
-     :tags ["api"]
 
-     (GET "/tweets" []
-       :query-params [user-name    :- s/Str
-                      {max-results :- s/Str 20}
-                      {bearer-token :- s/Str ENV-BEARER-TOKEN}]
-       :summary "success"
-       (ok {:result (fetch-recent-tweets user-name max-results bearer-token)})))))
-
-
+;; TODO - store data in both the dbs
+;; TODO - API to read tweets from the table
