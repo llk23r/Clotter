84026da8ea1bde8a18d3b4b360a7445eef1ca067
Rahul Acharya <ach@Smac.local>
2020-09-30 13:53:44 +0530

    feat(core): Handle Max-results limits

--- clotter/src/clotter/core.clj
+++ clotter/src/clotter/core.clj
@@ -10,9 +10,15 @@
             [ring.util.http-response :refer [ok not-found]]
             [clotter.handler :as handler]
             [clotter.models.tweet :refer [Tweet]]
-            [java-time :as t])
+            [java-time :as t]
+            [honeysql.format :as hformat])
   (:import (java.util Date))
   (:gen-class))
+
+
+(defmethod hformat/fn-handler "ilike"
+  [_ lhs rhs]
+  (format "(%s ilike %s)" (hformat/to-sql lhs) (hformat/to-sql rhs)))
 
 (def db-spec
   {:dbtype "postgres"
@@ -44,32 +50,42 @@
 (defn existing-tweet-ids [existing-tweets]
   (map :tweet_id existing-tweets))
 
-(defn tweet->response [tweet_ids]
+(defn tweet->response [tweet_ids max]
   (if tweet_ids
-    (ok {:result (db/select [Tweet :id :tweet_id :tweet_text :created_at] :tweet_id [:in tweet_ids])})
+    (let [db-tweets (db/select [Tweet :id :tweet_id :tweet_text :created_at] :tweet_id [:in tweet_ids] {:limit max})]
+      (ok {:total-tweets (count db-tweets)
+           :result db-tweets}))
     (not-found)))
+
+(defn max-results-resolver [max-results]
+  (if (or (cls/blank? max-results) (> (Integer. max-results) 100))
+    100
+    (if (< (Integer. max-results) 10)
+      10
+      (Integer. max-results))))
 
 (def fetch-tweets-route
   (GET "/tweets" []
     :query-params [user-name    :- s/Str
-                   {max-results :- s/Str 10}
+                   {max-results :- s/Str ""}
                    {bearer-token :- s/Str handler/ENV-BEARER-TOKEN}]
     :summary "Fetch New Tweets For The Given UserName"
-    (let [formatted-response (vec (formatted-response user-name max-results bearer-token))
+    (let [max (max-results-resolver max-results)
+          formatted-response (vec (formatted-response user-name max bearer-token))
           tweet-ids (tweet-ids formatted-response)
           existing-tweets (existing-tweets tweet-ids)
           existing-tweet-ids (set (existing-tweet-ids existing-tweets))]
       (-> (remove #(existing-tweet-ids (:tweet_id %)) formatted-response)
           (as-> new-tweets (map #(assoc % :user_name (str user-name)) new-tweets))
           (as-> tweets (db/insert-many! Tweet tweets)))
-      (tweet->response tweet-ids))))
+      (tweet->response tweet-ids max))))
 
 (defn string-to-date
   [date-string]
   (t/local-date "yyyy-MM-dd" date-string))
 
-(def now (t/local-date))
-(def yesterday (t/minus now (t/days 1)))
+(def today (t/plus (t/local-date) (t/days 1)))
+(def yesterday (t/minus today (t/days 1)))
 
 (defn start-date-resolver [start-date]
   (if (cls/blank? start-date)
@@ -78,20 +94,36 @@
 
 (defn end-date-resolver [end-date]
   (if (cls/blank? end-date)
-    now
+    today
     (string-to-date end-date)))
 
+(defn resolved-query [user-name sdate edate word max-tweets]
+  (if (and (cls/blank? word) (cls/blank? user-name))
+    []
+    (if (or (cls/blank? word) (cls/blank? user-name))
+      (if (cls/blank? word)
+        (db/select [Tweet :id :tweet_id :tweet_text :created_at] :created_at [:between sdate edate]
+                   :user_name [:ilike  user-name] {:limit max-tweets})
+        (db/select [Tweet :id :tweet_id :tweet_text :created_at] :created_at [:between sdate edate]
+                   :tweet_text [:ilike (str "%" word "%")] {:limit max-tweets}))
+      (db/select [Tweet :id :tweet_id :tweet_text :created_at] :created_at [:between sdate edate]
+                 :user_name [:ilike user-name], :tweet_text [:ilike (str "%" word "%")] {:limit max-tweets}))))
 
 (def search-tweets-route
   (GET "/tweets-search" []
-    :query-params [user-name :- s/Str
+    :query-params [{user-name :- s/Str ""}
                    {start-date :- s/Str ""}
-                   {end-date :- s/Str ""}]
+                   {end-date :- s/Str ""}
+                   {contains-word :- s/Str ""}
+                   {max-tweets :- s/Str 100}]
     :summary "Search for Tweets Data From the DB"
     (let [sdate (start-date-resolver start-date)
-          edate (end-date-resolver end-date)]
-      (ok {:result (db/select [Tweet :id :tweet_id :tweet_text :created_at] :created_at [:between sdate edate],
-                              :user_name [:= user-name])}))))
+          edate (end-date-resolver end-date)
+          word contains-word
+          max (Integer. max-tweets)
+          db-query-response (resolved-query user-name sdate edate word max)]
+      (ok {:total-tweets (count db-query-response)
+           :result db-query-response}))))
 
 
 (def app
