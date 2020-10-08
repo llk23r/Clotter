519b5e2a84461c755f85e33fa984aa8afec32f1b
Rahul Acharya <ach@Smac.local>
2020-10-08 17:58:24 +0530

    refactor(handler): Remove commented code

--- clotter/src/clotter/handler.clj
+++ clotter/src/clotter/handler.clj
@@ -20,27 +20,3 @@
   (let [query-params (str "from:" user-name " -is:retweet")
         response     (tweets-collection query-params max-results twitter-bearer-token)]
     (json/parse-string (get response :body))))
-
-
-;; (def app
-  ;; (api
-  ;;  {:swagger {:ui   "/"
-              ;; :spec "/swagger.json"
-              ;; :data {:info {:title       "Clotter"
-                            ;; :description "Compojure Api example"}
-                    ;;  :tags [{:name        "api"
-                            ;;  :description "some apis"}]}}}
-;; 
-  ;;  (context "/api" []
-    ;;  :tags ["api"]
-;; 
-    ;;  (GET "/tweets" []
-      ;;  :query-params [user-name    :- s/Str
-                      ;; {max-results :- s/Str 10}
-                      ;; {bearer-token :- s/Str ENV-BEARER-TOKEN}]
-      ;;  :summary "success"
-      ;;  (ok {:result (fetch-recent-tweets user-name max-results bearer-token)})))))
-
-
-;; TODO - store data in both the dbs
-;; TODO - API to read tweets from the table
